babf150640053ce00ad675f9c1db984c665b62c2
Rodrigo Cano <ioniviil@gmail.com>
2015-01-14 22:03:49 -0200

    added new flat classpath impl

--- scala.core/src/main/scala/org/netbeans/modules/scala/core/ScalaGlobal.scala
+++ scala.core/src/main/scala/org/netbeans/modules/scala/core/ScalaGlobal.scala
@@ -411,6 +411,8 @@ object ScalaGlobal {
       settings.verbose.value = false
     }
 
+    settings.YclasspathImpl.value = "flat"
+
     val bootCp = ClassPath.getClassPath(fo, ClassPath.BOOT)
     val compCp = ClassPath.getClassPath(fo, ClassPath.COMPILE)
     val srcCp = ClassPath.getClassPath(fo, ClassPath.SOURCE)
